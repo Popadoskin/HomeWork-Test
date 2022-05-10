2143b3900bcae559880f412406d8c1a4da11234b
Aleksandr <popados812@mail.ru>
2022-05-10 15:21:14 +0300

    Подсчет размера нового массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,3 +1,25 @@
-﻿string[] array = { "1234", "ask", "sun", "hello", "world", "sea", "12" };
+﻿string[] CreateArray(string[] initialArray, int lengthOfWord)
+{
+    int length = new int();
+    for (int i = 0; i < initialArray.Length; i++)
+    {
+       if(initialArray[i].Length <= lengthOfWord)
+       {
+           length ++;
+       } 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+string[] array = { "1234", "ask", "sun", "hello", "world", "sea", "12" };
 string[] newArray = CreateArray();
 PrintArray(newArray);
